b77736e2de91aa0fe340fdecc39ecd61c27609f5
Rasmus <hi@rasm.io>
2016-02-14 12:19:46 +0100

    init with api

--- web/index.php
+++ web/index.php
@@ -1,9 +1,52 @@
 <?php
 
 require('../vendor/autoload.php');
+require_once "./ClashAPI/API.class.php";
 
 $app = new Silex\Application();
 $app['debug'] = true;
+
+
+$clan = new CoC_Clan("#QVQRYYG");
+$clandetails = [
+  "badgesm" =>  $clan->getBadgeUrl("small"),
+  "badgemd" =>  $clan->getBadgeUrl("medium"),
+  "badgelg" =>  $clan->getBadgeUrl("large"),
+  "name" => $clan->getName(),
+  "level" => $clan->getLevel(),
+  "description" => $clan->getDescription(),
+];
+
+foreach ($clan->getAllMembers() as $clanmember)
+{
+	$member = new CoC_Member($clanmember);
+  $league = new CoC_League($member->getLeague());
+  $donationsReceivedCalc = $member->getDonationsReceived();
+	if ($donationsReceivedCalc == 0) $donationsReceivedCalc++;
+
+	$ratio = $member->getDonations() / $donationsReceivedCalc;
+
+  $clanmem[$member->getClanRank()] = [
+    "rank" => $member->getClanRank(),
+    "name" => $member->getName(),
+    "role" => $member->getRole(),
+    "trophies" => $member->getTrophies(),
+    "donations" => $member->getDonations(),
+    "received" => $member->getDonationsReceived(),
+    "ratio" => number_format($ratio, 2),
+    "level" => $member->getLevel(),
+    "leaguename" => $league->getLeagueName(),
+    "leagueicontn" => $league->getLeagueIcon("tiny"),
+    "leagueiconsm" => $league->getLeagueIcon("small"),
+    "leagueiconmd" => $league->getLeagueIcon("medium"),
+  ];
+
+}
+
+
+
+$app['clandetails'] = $clandetails;
+$app['clanmem'] = $clanmem;
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
@@ -20,6 +63,11 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
+});
+
+$app->get('/clan', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('clan.twig');
 });
 
 $app->run();
