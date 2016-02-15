5b28210d5b70a223706293b0a16fbe23537df0d6
rasmus <hi@rasm.io>
2016-02-15 09:09:46 +0100

    redo

--- web/index.php
+++ web/index.php
@@ -113,6 +113,6 @@ if ($app['debug']) {
  $app->run();
  }
  else{
-Request::setTrustedProxies(array($request->server->get('REMOTE_ADDR')));
+Request::setTrustedProxies(array('127.0.0.1'));
  $app['http_cache']->run();
  }
